6c627d14c998ee57fb1217c3564d5338a1643dd7
Yifei Yang <958751036@qq.com>
2019-08-17 07:39:56 +0800

    add class loading sequence code

---
+++ src/main/java/com/yifeiyang/classLoader/ClassLoaderMain.java
@@ -0,0 +1,11 @@
+package com.yifeiyang.classLoader;
+
+/***
+ * Specify -verbose:class as JVM option to see class loading sequence
+ */
+public class ClassLoaderMain {
+    public static void main(String[] args) {
+        System.out.println("Before call Math.random()");
+        System.out.println(Math.random());
+    }
+}
