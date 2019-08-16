85b67115168662dd271833e768599858380dda4d
Yifei Yang <958751036@qq.com>
2019-08-17 07:41:54 +0800

    refactor code

--- src/main/java/com/yifeiyang/classLoader/ClassLoaderSequenceMain.java
+++ src/main/java/com/yifeiyang/classLoader/ClassLoaderSequenceMain.java
@@ -3,7 +3,7 @@ package com.yifeiyang.classLoader;
 /***
  * Specify -verbose:class as JVM option to see class loading sequence
  */
-public class ClassLoaderMain {
+public class ClassLoaderSequenceMain {
     public static void main(String[] args) {
         System.out.println("Before call Math.random()");
         System.out.println(Math.random());
